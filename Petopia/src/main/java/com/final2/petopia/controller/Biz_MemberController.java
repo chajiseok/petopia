9f1a94fd15469adf41cbbb9b28b2241bd14b1b4d
user1 <user1@DESKTOP-UKNJ66C>
2019-01-09 18:38:57 +0900

    2019-01-09 기업회원가입하는중~

--- Petopia/src/main/java/com/final2/petopia/controller/Biz_MemberController.java
+++ Petopia/src/main/java/com/final2/petopia/controller/Biz_MemberController.java
@@ -1,8 +1,16 @@
 package com.final2.petopia.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Biz_MemberController {
+	
+	@RequestMapping(value="/joinBizMember.pet", method={RequestMethod.GET})
+	public String joinBizMember() {
+		
+		return "join/joinBizMember.tiles1";
+	} // end of public String joinBizMember()
 
 }
